836b31fde0aae7a9776c56d1c330819a204a34d1
FortunesFavored <gregory.j.maj@gmail.com>
2021-02-10 17:19:27 -0600

    first commit -GM

---
+++ fabulous.py
@@ -0,0 +1,10 @@
+import math
+
+def get_area():
+    w = int(input('What is the width of your house? '))
+    l = int(input('What is the length of your house? '))
+    return w*l
+
+def get_area_circ():
+    r = int(input('What is the radius? '))
+    return math.pi*r**2
